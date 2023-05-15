82719662228fb6e2fd0eea18e81de1cb3f81ce37
Adhitya Yosua Sanjaya <adhityasnjy@gmail.com>
2023-05-15 11:49:18 +0700

    implement i18n on metadata description

---
+++ i18n/getDictionary.ts
@@ -0,0 +1,9 @@
+import 'server-only'
+import type { Locale } from './config'
+
+const dictionaries = {
+  en: () => import('./dictionaries/en.json').then((module) => module.default),
+  id: () => import('./dictionaries/id.json').then((module) => module.default),
+}
+
+export const getDictionary = async (locale: Locale) => dictionaries[locale]()
