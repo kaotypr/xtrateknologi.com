3d3c1cabf4c84aca5b5b75d337a500f0de755850
Adhitya Yosua Sanjaya <adhityasnjy@gmail.com>
2023-05-24 21:01:49 +0700

    solutions & teams constant data-assets

--- constants/teams.ts
+++ constants/teams.ts
@@ -2,41 +2,41 @@ export const XTRA_TEAM = [
   {
     name: 'Awal Guntara',
     position: 'Founder & Executive',
-    photo: '/static/images/awal-guntara.png',
+    photo: '/static/images/teams/awal-guntara.png',
   },
   {
     name: 'Eki Ariawan',
     position: 'Legal Officer',
-    photo: '/static/images/eki-ariawan.png',
+    photo: '/static/images/teams/eki-ariawan.png',
   },
   {
     name: 'Sandi Susantor',
     position: 'IT Consultant',
-    photo: '/static/images/sandi-susanto.png',
+    photo: '/static/images/teams/sandi-susanto.png',
   },
   {
     name: 'Mufti Ali',
     position: 'Marketing',
-    photo: '/static/images/mufti-ali.png',
+    photo: '/static/images/teams/mufti-ali.png',
   },
   {
     name: 'Muhammad Iqbal',
     position: 'Designer & RnD',
-    photo: '/static/images/muhammad-iqbal.png',
+    photo: '/static/images/teams/muhammad-iqbal.png',
   },
   {
     name: 'Galang Pratama',
     position: 'Software Engineer',
-    photo: '/static/images/galang-pratama.png',
+    photo: '/static/images/teams/galang-pratama.png',
   },
   {
     name: 'Eko Setyo',
     position: 'IoT Engineer',
-    photo: '/static/images/eko-setyo.png',
+    photo: '/static/images/teams/eko-setyo.png',
   },
   {
     name: 'Adhitya Sanjaya',
     position: 'Software Eng ing eng',
-    photo: '/static/images/adhitya-sanjaya.png',
+    photo: '/static/images/teams/adhitya-sanjaya.png',
   },
 ]
