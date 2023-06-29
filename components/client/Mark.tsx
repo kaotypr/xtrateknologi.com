e64226fdc7417f503dde6e62095a3549f9391a9b
Adhitya Yosua Sanjaya <adhityasnjy@gmail.com>
2023-06-30 01:16:30 +0700

    changes: @kaotypr as title

--- components/client/Mark.tsx
+++ components/client/Mark.tsx
@@ -6,16 +6,14 @@ export default function Mark() {
   return (
     <div
       id="all-rights-reserved-and-developer"
-      className="px-4 py-1 sm:px-10 lg:px-20 absolute bottom-0 w-full bg-gradient-to-tr from-primary-100 to-primary-80 flex flex-row flex-wrap text-center justify-center md:justify-between text-sm font-normal"
+      className="px-4 py-1 sm:px-10 lg:px-20 absolute bottom-0 w-full bg-gradient-to-tr from-primary-100 to-primary-80 flex flex-row flex-wrap text-center justify-center md:justify-center text-sm font-normal"
     >
       <Link className="block" href={atob(Z2l0aHVi)}>
-        <span className="text-center hover:text-primary-20 text-[0.8rem]">
+        <span
+          title={atob(dXNlcm5hbWU)}
+          className="text-center hover:text-primary-20 text-[0.8rem]"
+        >
           © 2023 PT. Xtra Teknologi Solusi. All rights reserved
-        </span>
-      </Link>
-      <Link className="ml-2 block" href={atob(Z2l0aHVi)}>
-        <span className="hover:text-primary-20 text-[0.8rem]">
-          {atob(dXNlcm5hbWU)}
         </span>
       </Link>
     </div>
